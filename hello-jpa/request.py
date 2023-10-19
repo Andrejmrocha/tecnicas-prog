309ca76959a65c55d746b40c836958583e51aa62
André Jonas <104024792+Andrejmrocha@users.noreply.github.com>
2023-10-19 20:38:05 -0300

    Add files via upload

---
+++ hello-jpa/request.py
@@ -0,0 +1,25 @@
+import requests
+
+if __name__ == "__main__":
+    body = {
+        "name" : "pen",
+        "description" : "black pen",
+        "price": 2.5 
+    }
+
+    body2 = {
+        "name" : "pencil",
+        "description" : "blue pen",
+        "price": 2 
+    }
+
+    excluir = {
+        "name" : "pen"
+    }
+    #response = requests.post("http://localhost:8080/products", body)
+    #response2 = requests.post("http://localhost:8080/products", body2)
+    # response = requests.delete("http://localhost:8080/products", excluir)
+
+    response3 = requests.get("http://localhost:8080/products/pen")
+    print(response3.text)
+    
